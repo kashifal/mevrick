7cc33e1ff3e2957435f9c6b54762edc30e04c061
kashifal <captainkashi4@gmail.com>
2024-04-07 23:45:19 +0500

    fixes

--- components/ProgressBar.vue
+++ components/ProgressBar.vue
@@ -1,12 +1,6 @@
 <template>
-  <div v-if="left || right === true" class="progress-bar">
-    <div
-      :class="[`filled-bar ${filled}`]"
-      :style="{
-        transform: `translate3d(-${(1 - value) * 100}%, 0, 0)`,
-        width: `${value * 100}%`,
-      }"
-    ></div>
+  <div v-if="left || right" class="progress-bar">
+    <div :class="[`filled-bar ${filled}`]" :style="barStyle"></div>
     <!-- <span class="text">Progress: {{ left }}</span> -->
   </div>
 </template>
@@ -16,25 +10,33 @@ import { computed } from "vue";
 
 const props = defineProps(["value", "right", "left", "filled"]);
 
+const barStyle = computed(() => ({
+  transform: `translate3d(-${(1 - props.value) * 100}%, 0, 0)`,
+  width: `${props.value * 100}%`,
+}));
+
 const percentageText = computed(() => `${Math.round(props.value * 100)}%`);
 </script>
 
 <style scoped>
+.progress-bar {
+  position: relative;
+  height: 100%;
+}
+
 .filled-bar {
   position: absolute;
   top: 0;
   left: 0;
-
   height: 100%;
-  width: 100%;
-  transform: translate3d(-100%, 0, 0);
-
   z-index: 12000;
+  transition: transform 0.6s ease-in-out; /* Add smooth transition for transform */
 }
-/* Your ProgressBar component styles here */
+
 .blue {
   background-color: #10435d !important;
 }
+
 .gray {
   background-color: #444444 !important;
 }
